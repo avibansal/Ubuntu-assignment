fc041ce7b66934fc1cda92c01d5fd4da1933aea4
Avi bansal <33563429+avibansal@users.noreply.github.com>
2024-03-09 21:20:25 +0530

    Create 12.sh

---
+++ 12.sh
@@ -0,0 +1,11 @@
+chown tom project-{1..4}
+
+ chgrp dac tmp/iacsd/project-{1..4}
+ chgrp dac project-{1..4}
+
+ chgrp dac project-{1..4}
+ ls -l
+ chmod 600 project-{1..4}
+ ls -l
+ chmod 607 project-{1..4}
+ ls -l
