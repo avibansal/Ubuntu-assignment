dd3f4bdb2085579cd3f50380fe11811823eedb53
Avi bansal <33563429+avibansal@users.noreply.github.com>
2024-03-09 20:20:18 +0530

    Create 8-10.sh

---
+++ 8-10.sh
@@ -0,0 +1,11 @@
+    1  sudo adduser tom
+    2  sudo adduser bob
+    3  sudo adduser sam
+    4  sudo adduser prince
+    5  sudo addgroup dac
+    6  sudo addgroup dbda
+    7  sudo addgroup ditiss
+    8  sudo adduser tom --gid 1005
+    9  sudo adduser tom dac
+   10  sudo adduser bob dbda
+   11  sudo adduser sam ditiss
